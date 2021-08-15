e70fdf4aa1de962923fca8888283d90bc6f2cc11
Filipe Bezerra <filipebzerra@gmail.com>
2021-08-15 14:50:01 -0300

    Include all materials for lesson one

---
+++ 001-lesson-one--hello-android/README.md
@@ -0,0 +1,19 @@
+Lesson One - Hello Android
+==========================
+
+In this lesson we have a high level overview on how to write Kotlin code for a Hello Android application.
+
+Specifically we demonstrate how an Android project is created using Android Studio, structured with important configuration files like `gradle.build` and `AndroidManifest.xml` and in the end we learn how to create Kotlin functions to be invoked by Android code.
+
+Code activity
+-------------
+In this code activity you are challenged to modify the demo created in this lesson and create a function with a single argument and additionally with documentation.
+
+Step by step:
+1. Open Android Studio and click in **Open an Existing Project**
+2. Navigate to the folder where [hello-android-initial-code](./hello-android-initial-code) is located, select it and click **OK**
+3. Wait for the **build** process to complete
+4. Open the file [SecondFragment.kt](./hello-android-initial-code/app/src/main/java/com/android/example/hello/SecondFragment.kt)
+5. There's **2 TODO's** for you to complete as the challenge
+6. When you finish to implement **both TODO's**, on Android Studio click Run 'app' <img src="../images/studio/buttons/toolbar-run.png" style="height: 1.2em;vertical-align: sub;">.
+7. After completed or if you have questions you can check out the final solution at [hello-android-solution-code](./hello-android-solution-code)
---
+++ 001-lesson-one--hello-android/hello-android-solution-code/app/src/main/java/com/android/example/hello/SecondFragment.kt
@@ -0,0 +1,69 @@
+package com.android.example.hello
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.android.example.hello.databinding.FragmentSecondBinding
+import com.google.android.material.snackbar.Snackbar
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class SecondFragment : Fragment() {
+
+    private var _binding: FragmentSecondBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonSecond.setOnClickListener {
+            // 2 - Invoke function showMessage() passing the textMessage argument to show the message to the user
+            showMessage("Hello Android")
+        }
+    }
+
+    // 1 - Create the function showMessage() with one argument which receives a text.
+    //  What's the correct data type to use to show this text message?
+    //  Additionally declare the documentation using the function showSnackbar(text: String) as a example
+    /**
+     * Show a text message to the user.
+     * @param textMessage: The text message to show to the user.
+     */
+    fun showMessage(textMessage: String) {
+        showSnackbar(textMessage)
+    }
+
+    /**
+     * Show a [Snackbar] and then execute the defined action.
+     * @param text The text to show.
+     */
+    private fun showSnackbar(text: String) {
+        Snackbar.make(binding.root, text, Snackbar.LENGTH_INDEFINITE)
+            .setAction("Previous") {
+                findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+            }
+            .show()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
